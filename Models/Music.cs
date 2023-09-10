46c7a769a5b0f86c4e84d94707cb4210b1899cb5
Iagor M.L <iagorluchesi@gmail.com>
2023-09-10 11:52:30 -0400

    added try catch and music class model

---
+++ Models/Music.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace api_request.Models
+{
+    internal class Music
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("artist")]
+        public string? Artist { get; set; }
+
+        [JsonPropertyName("duration_ms")]
+        public int? Duration { get; set; }
+
+        [JsonPropertyName("gender")]
+        public string? Gender { get; set; }
+
+        public void ShowMusicDetails()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Artist);
+            Console.WriteLine(Duration);
+            Console.WriteLine(Gender);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace api_request
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+                Console.WriteLine(response);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
